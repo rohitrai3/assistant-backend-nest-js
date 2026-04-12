57ea79c38f5f2e06194cf3fef7f49d2451ffe480
Rohit Rai <rohitraijob@gmail.com>
2026-04-12 17:39:15 +0530

    feat: Get PORT from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule],
+  imports: [ConfigModule.forRoot(), EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

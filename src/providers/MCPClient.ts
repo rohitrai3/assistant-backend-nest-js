9fd22262bd76110055bc3792d3cc685c6ea77767
Rohit Rai <rohitraijob@gmail.com>
2026-05-16 10:02:49 +0530

    chore: Remove ConfigService usage in McpClient

--- src/providers/MCPClient.ts
+++ src/providers/MCPClient.ts
@@ -22,7 +22,6 @@ import TtsModel from './TTS';
 import { join } from 'path';
 import { readdir, readFile } from 'fs/promises';
 import { ToolResultBlockParam } from '@anthropic-ai/sdk/resources';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export default class McpClient {
@@ -35,7 +34,7 @@ export default class McpClient {
   @Inject()
   private ttsModel: TtsModel;
 
-  constructor(private configService: ConfigService) {
+  constructor() {
     this.logger.log('Initialize McpClient');
     this.mcp = new Client({ name: 'mcp-client-cli', version: '1.0.0' });
     this.clients = new Map();
@@ -115,7 +114,7 @@ export default class McpClient {
   }
 
   private async callLlm(server: Server, isSynthesize: boolean) {
-    await fetch(`${this.configService.get<string>('LLM_URL')}/v1/messages`, {
+    await fetch(`${process.env.LLM_URL}/v1/messages`, {
       method: 'POST',
       headers: {
         'Content-Type': 'text/event-stream',
